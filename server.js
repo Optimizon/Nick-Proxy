dd5bd52538b08ad442b55805a6440711672cf21c
nick567187 <nchang.edu@gmail.com>
2018-10-11 19:03:59 -0700

    proxy

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 const fetch = require('node-fetch');
 
 
@@ -13,10 +13,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/api/reviews/:productId', (req, res) => {
   fetch(`http://ec2-18-144-29-61.us-west-1.compute.amazonaws.com/api/reviews/${req.params.productId}`)
-    .then((res) => {
-    	console.log(res);
-      return res.json();
-    })
+    .then((res) => res.json())
     .then(json => res.send(json));
 });
 
@@ -33,43 +30,28 @@ app.post('/api/reviews/:productId', (req, res) => {
 // sonia
 
 app.get('/product', (req, res) => { //will be sent to my component 
-  // client.get(req.query.id, function(err, data) {
-  //   console.log("here~")
-  //   if (err) throw err;
-  //   if (data != null) {
-  //       console.log("hit not null")
-  //       console.log(typeof JSON.parse(data));
-  //       res.send({"data": JSON.parse(data).rows});
-  //   } else {
-    fetch(`http://ec2-52-53-250-252.us-west-1.compute.amazonaws.com:4043/product/?id=${req.query.id}`) //will proxy to my component server 
-      .then((res) => {
-        return res.json();
-      })
+  fetch(`http://ec2-52-53-250-252.us-west-1.compute.amazonaws.com:4043/product/?id=${req.query.id}`) //will proxy to my component server 
+      .then(res => res.json())
       .then(json => res.send(JSON.stringify(json)));
-   //}
-  })
+  });
 
 // michelle
 
 app.get('/get', (req, res) => {
   fetch(`http://ec2-54-153-66-98.us-west-1.compute.amazonaws.com:9001/get/?id=${req.query.id}`)
-    .then(response => {
-      return response.json()
-    }).then(json => {
-      res.send(json)
-    })
+    .then(response => response.json())
+    .then(json => res.send(json))
     .catch(err => console.log(err))
 });
 
 // matt
 
-app.get('/api/checkout/:productId', (req, res) => {
-  fetch(`http://localhost:7777/checkout/${req.params.productId}`)
-    .then((res) => {
-      return res.json();
-    })
-    .then(json => res.send(json));
+app.get(`/checkout/:productId`, (req, res) => {
+  fetch(`http://ec2-18-212-122-128.compute-1.amazonaws.com:7777/checkout/${req.params.productId}`)
+   .then(res => res.json())
+   .then(json => res.send(json));
 });
+
 app.listen(port, () => {
   console.log(`Proxy server listening on port ${port}...`);
 });
